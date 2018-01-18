f5dada77491b335fc44da67e6547b19d6a9ac671
jlo4 <john.love4@gmail.com>
2018-01-18 16:36:59 +0200

    add edit and delete notes

--- src/containers/NewNote.js
+++ src/containers/NewNote.js
@@ -3,6 +3,7 @@ import { FormGroup, FormControl, ControlLabel } from 'react-bootstrap'
 import LoaderButton from '../components/LoaderButton'
 import config from '../config'
 import './NewNote.css'
+import { invokeApig, s3Upload } from '../libs/awsLib'
 
 export default class NewNote extends Component {
   constructor(props) {
@@ -39,6 +40,30 @@ export default class NewNote extends Component {
     }
     
     this.setState({ isLoading: true })
+    
+    
+    try {
+      const uploadedFilename = this.file
+        ? (await s3Upload(this.file)).Location
+        : null
+
+      await this.createNote({
+        content: this.state.content,
+        attachment: uploadedFilename
+      })
+      this.props.history.push('/')
+    } catch (e) {
+      alert(e)
+      this.setState({ isLoading: false })
+    }
+  }
+
+  createNote(note) {
+    return invokeApig({
+      path: '/notes',
+      method: 'POST',
+      body: note
+    })
   }
 
   render() {
